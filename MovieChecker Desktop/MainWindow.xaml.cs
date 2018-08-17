619290f2cd92b9d6dd7ea333a26d9bfe6f9d37e0
GKaszewski <gabrielkaszewski@gmail.com>
2018-08-17 23:05:24 +0200

    Removed unused usings.

--- MovieChecker Desktop/MainWindow.xaml.cs
+++ MovieChecker Desktop/MainWindow.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MovieChecker_Desktop.Classes;
 using TMDbLib.Client;
-using TMDbLib.Objects.General;
-using TMDbLib.Objects.Genres;
 using TMDbLib.Objects.Movies;
 using TMDbLib.Objects.Search;
 
