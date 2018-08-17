38d789aa96bbe12df1810a5462de4c5ae659e3b6
GKaszewski <gabrielkaszewski@gmail.com>
2018-08-17 22:03:23 +0200

    Added Loader class.

---
+++ MovieChecker Desktop/Classes/Loader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace MovieChecker_Desktop.Classes
+{
+    public static class Loader
+    {
+        public static string LoadTextFile(string path)
+        {
+            using (var fileStream = File.OpenRead(path))
+            {
+                StreamReader streamReader = new StreamReader(fileStream);
+                string text = streamReader.ReadToEnd();
+
+                return text;
+            }
+        }
+    }
+}
